1e241ad083ac276d6d1c0220123702675abf5610
Gabriel Barros <gabrielbarross2311@gmail.com>
2023-04-18 21:07:57 -0300

    sistema de Loading adicionado na pagina Favorites.

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -4,9 +4,9 @@ import { useAuth } from '../../hooks/auth'
 import { api } from '../../services/api'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import { RxCaretLeft } from 'react-icons/rx'
 import { useNavigate } from 'react-router-dom'
 import { HiEmojiSad } from 'react-icons/hi'
+import { Ring } from '@uiball/loaders'
 
 
 export function Favorites() {
@@ -15,6 +15,7 @@ export function Favorites() {
 
     const [dishes, setDishes] = useState([])
     const [updateFavoritesTrigger, setUpdateFavoritesTrigger] = useState(1)
+    const [loading, setLoading] = useState(true)
 
     const navigate = useNavigate()
 
@@ -23,24 +24,42 @@ export function Favorites() {
     }
 
     async function handleGetFavorites() {
+        try{
 
-        const userFavoritesDishes = await favorites.GetFavoritesByUserId().then(response => response.data)
+            const userFavoritesDishes = await favorites.GetFavoritesByUserId().then(response => response.data)
+    
+            const dish_id = userFavoritesDishes.map(favorite => favorite.dish_id)
+    
+            const dishesPromise = dish_id.map(id => api.get(`/dishes/${id}`))
+    
+            const dishes = await Promise.all(dishesPromise).then(response => response.map(response => response.data))
+    
+            setDishes(dishes)
+            setLoading(false)
+        }catch (error) {
+            if (error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert('Não foi possível buscar os favoritos')
+            }
+        }
 
-        const dish_id = userFavoritesDishes.map(favorite => favorite.dish_id)
-
-        const dishesPromise = dish_id.map(id => api.get(`/dishes/${id}`))
-
-        const dishes = await Promise.all(dishesPromise).then(response => response.map(response => response.data))
-
-        setDishes(dishes)
     }
 
     const handleRemoveFavorite = async (dish_id) => {
-        const userFavoritesDishes = await favorites.GetFavoritesByUserId().then((response) => response.data)
-        const Dish = userFavoritesDishes.find(favorite => favorite.dish_id === dish_id)
-
-        await favorites.DeleteFavorites(Dish.id)
-        setUpdateFavoritesTrigger(prevState => prevState + 1)
+        try{
+            const userFavoritesDishes = await favorites.GetFavoritesByUserId().then((response) => response.data)
+            const Dish = userFavoritesDishes.find(favorite => favorite.dish_id === dish_id)
+    
+            await favorites.DeleteFavorites(Dish.id)
+            setUpdateFavoritesTrigger(prevState => prevState + 1)
+        }catch(error){
+            if(error.response){
+                alert(error.response.data.message)
+            }else{
+                alert('Não foi possível remover o prato dos favoritos')
+            }
+        }
     }
 
     useEffect(() => {
@@ -56,16 +75,25 @@ export function Favorites() {
                 <BackButton onClick={handleBack}/>
                 <h1>Meus Favoritos</h1>
 
-                {dishes.length == 0 ?
-
+                {
+                loading ?
+                    <Ring
+                    size={60}
+                    lineWeight={5}
+                    speed={2}
+                    color="white"
+                    />
+                :
+                dishes.length == 0 ? 
                     <h2>Nenhum prato favorito <HiEmojiSad /></h2>
-                    :
+                :
                     <li>
                         {dishes && dishes.map((dish) => (
                             <DishesFavorites dish={dish} key={dish.id} removeFunction={handleRemoveFavorite} />
                         ))}
                     </li>
                 }
+
 
 
             </Content>
